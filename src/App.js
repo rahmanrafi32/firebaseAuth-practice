06c174e32e3c7186262b7926018314614693859e
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2021-03-18 15:40:17 +0600

    user signUp fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import firebaseConfig from './firebaseConfig';
+
+!firebase.apps.length && firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user,setUser] = useState({success: false})
+  const [newUser,setNewUser] = useState()
+  //google sign in 
+  const googleSignIn = ()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      const user = result.user;
+      setUser(user);
+    }).catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  }
+  //handeling informations from input
+  const handleBlur = (e)=>{
+    let validField = true;
+    if(e.target.name === 'email'){
+      const validField = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if(e.target.name === 'password'){
+      const validField = /(?=.*[0-9])(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?=\\S+$)/.test(e.target.value)
+    }
+    if(validField){
+      const newUser = {...user};
+      newUser[e.target.name]=e.target.value;
+      setUser(newUser);
+    }
+    
+  }
+
+  //sign up new user
+  const signUp = (e)=>{
+    e.preventDefault();
+    if(user.email && user.password){
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const newUser = {...user}
+        newUser.error = '';
+        newUser.success = true;
+        setUser(newUser);
+
+      })
+      .catch((error) => {
+        const newUser = {...user}
+        newUser.error = error.message;
+        newUser.success = false;
+        setUser(newUser);
+      });
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* google signIn */}
+      <>
+        <button onClick={googleSignIn}>Google SignI</button>
+        <br/>
+        <h3>Welcome: {user.displayName}</h3>
+      </>
+      {/* signUp with rendom eamil id an password */}
+      <>
+        <form onSubmit ={signUp}>
+          <input type="text" onBlur={handleBlur} name="email" placeholder='enter your email' required/>
+          <br/>
+          <input type="password" onBlur={handleBlur} name="password" placeholder='enter your password' required/>
+          <br/>
+          <input type="submit" value="Sign Up"/>
+        </form>
+        {
+          user.success ? <p>Sign Up successful</p> : <p>{user.error}</p>
+        }
+      </>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5BrH2avPMt2_2iU5DQfQW_Ucn4Y65R-4",
+    authDomain: "practice-project-823262.firebaseapp.com",
+    projectId: "practice-project-823262",
+    storageBucket: "practice-project-823262.appspot.com",
+    messagingSenderId: "824752430697",
+    appId: "1:824752430697:web:d07d182ed4d62b5800ccf8"
+  };
+
+  export default firebaseConfig;
